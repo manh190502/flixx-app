73d1f714b42626573ce4ee160384a786af6c5c25
Cong Manh <code.manh1905@gmail.com>
2024-08-15 17:38:22 +0700

    added tvshow-details

--- js/script.js
+++ js/script.js
@@ -34,7 +34,7 @@ function init() {
       displayMovieDetails();
       break;
     case "/tv-details.html":
-      console.log("TV Details");
+      displayShowDetails();
       break;
     case "/search.html":
       console.log("Search");
@@ -87,7 +87,7 @@ async function displayPopularShows() {
     const div = document.createElement("div");
     div.classList.add("card");
     div.innerHTML = `
-                <a href="show-details.html?id=${show.id}">
+                <a href="tv-details.html?id=${show.id}">
                   ${
                     show.poster_path
                       ? `<img
@@ -135,12 +135,12 @@ async function displayMovieDetails() {
                     ? `              <img
                 src="https://image.tmdb.org/t/p/w500${movie.poster_path}"
                 class="card-img-top"
-                alt="Movie Title"
+                alt="${movie.title}"
               />`
                     : `              <img
                 src="images/no-image.jpg"
                 class="card-img-top"
-                alt="Movie Title"
+                alt="${movie.title}"
               />`
                 } 
             </div>
@@ -189,6 +189,76 @@ async function displayMovieDetails() {
     `;
 
   document.querySelector("#movie-details").appendChild(div);
+}
+
+// Display tvshow details
+
+async function displayShowDetails() {
+  const tvID = window.location.search.split("=")[1];
+  const tv = await fetchAPIData(`tv/${tvID}`);
+
+  //   overlay for background image
+  displayBackgroundImage("show", tv.backdrop_path);
+
+  const div = document.createElement("div");
+
+  div.innerHTML = `
+               <div class="details-top">
+          <div>
+            ${
+              tv.poster_path
+                ? `            <img
+              src="https://image.tmdb.org/t/p/w500${tv.poster_path}"
+              class="card-img-top"
+              alt="${tv.name}"
+            />`
+                : `            <img
+              src="images/no-image.jpg"
+              class="card-img-top"
+              alt="${tv.name}"
+            />`
+            }
+          </div>
+          <div>
+            <h2>${tv.name}</h2>
+            <p>
+              <i class="fas fa-star text-primary"></i>
+              ${tv.vote_average} / 10
+            </p>
+            <p class="text-muted">Release Date: ${tv.first_air_date}</p>
+            <p>
+                ${tv.overview}
+            </p>
+            <h5>Genres</h5>
+            <ul class="list-group">
+              ${tv.genres.map((genre) => `${genre.name}`).join("")}
+            </ul>
+            <a href="${
+              tv.homepage
+            }" target="_blank" class="btn">Visit Show Homepage</a>
+          </div>
+        </div>
+        <div class="details-bottom">
+          <h2>Show Info</h2>
+          <ul>
+            <li><span class="text-secondary">Number Of Episodes:</span> ${
+              tv.number_of_episodes
+            }</li>
+            <li>
+              <span class="text-secondary">Last Episode To Air:</span> ${
+                tv.last_episode_to_air.name
+              }
+            </li>
+            <li><span class="text-secondary">Status:</span> ${tv.status}</li>
+          </ul>
+          <h4>Production Companies</h4>
+          <div class="list-group">${tv.production_companies.map(
+            (company) => `${company.name}`
+          )}</div>
+        </div>
+      `;
+
+  document.querySelector("#show-details").appendChild(div);
 }
 
 // add commas to number
